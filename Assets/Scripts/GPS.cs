ed7f0a4725ffce6ec285ba5248b4133e9a6d1181
Fuzzyundies <Eric.Jonathon.Lambert@gmail.com>
2018-03-20 02:56:32 +0000

    GPS and Assets
    Troubleshooting getting the GPS to update
    
    Added basic assets.

---
+++ Assets/Scripts/GPS.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPS : MonoBehaviour
+{
+    public static GPS Instance { set; get; }
+
+    public float latitude, longitude;
+
+    int maxWait = 20;
+    // Use this for initialization
+    void Start ()
+    {
+        //System takes care of requesting user
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        StartCoroutine(StartLocationService());
+
+	}
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+
+    private IEnumerator StartLocationService()
+    {
+        if (!Input.location.isEnabledByUser)
+        {
+            Debug.Log("User has not enabled GPS");
+            yield break;
+        }
+
+        Input.location.Start();
+
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        if (maxWait <= 0)
+        {
+            Debug.Log("Request timed out");
+            yield break;
+        }
+
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            Debug.Log("Unable to determine device location");
+            yield break;
+        }
+
+        latitude = Input.location.lastData.latitude;
+        longitude = Input.location.lastData.longitude;
+    }
+}
---
+++ Assets/Scripts/UpdateGPSText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateGPSText: MonoBehaviour
+{
+    public Text coordiantes;
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        coordiantes.text = ("Lat:" + GPS.Instance.latitude.ToString() + "   Long:" + GPS.Instance.longitude.ToString());
+    }
+}
