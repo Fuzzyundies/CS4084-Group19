3d3fe4a01f6c0afe823bcd22a001f756109dcd1a
Fuzzyundies <Eric.Jonathon.Lambert@gmail.com>
2018-04-03 14:22:14 +0100

    Additional Scenes
    Added additional scenes with the ability to navigate through each. Currently Combat and Player select are bypassed as the enemies and the multiple players has not been implemented yet.

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    public void ChangeToScene(int sceneToChangeTo)
+    {
+        SceneManager.LoadScene(sceneToChangeTo);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
