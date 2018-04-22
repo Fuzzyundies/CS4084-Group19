3f752319de65e986ba9161e75d94bef4bc4b7996
Fuzzyundies <Eric.Jonathon.Lambert@gmail.com>
2018-04-22 13:48:13 +0100

    Updated Map
    Mapbox did not work, with items no longer supported.
    Bought and implemented Go Maps, and the tutorial

--- Assets/Mapbox/Core/mapbox-sdk-cs/Tests/UnitTests/Editor/TileResourceTest.cs
+++ Assets/Mapbox/Core/mapbox-sdk-cs/Tests/UnitTests/Editor/TileResourceTest.cs
@@ -5,7 +5,6 @@
 //-----------------------------------------------------------------------
 
 // TODO: figure out how run tests outside of Unity with .NET framework, something like '#if !UNITY'
-#if UNITY_EDITOR
 #if UNITY_5_6_OR_NEWER
 
 namespace Mapbox.MapboxSdkCs.UnitTest
@@ -18,69 +17,62 @@ namespace Mapbox.MapboxSdkCs.UnitTest
 	[TestFixture]
 	public class TileResourceTest
 	{
-		//needed when tests can be enabled again
-		//private string _api;
-		//private CanonicalTileId _tileId;
+
+		private string _api;
+		private CanonicalTileId _tileId;
+
 
 		[SetUp]
 		public void SetUp()
 		{
-			//_api = Constants.BaseAPI;
-			//_tileId = new CanonicalTileId(0, 0, 0);
+			_api = Constants.BaseAPI;
+			_tileId = new CanonicalTileId(0, 0, 0);
 		}
 
+
 		[Test]
-		[Ignore("not working in Unity as 'TileResource' is 'internal sealed' and mapbox-sdk-cs and tests end up in 2 different aseemblies")]
 		public void GetUrlRaster()
 		{
-			// TODO
-			/*
 			var res1 = TileResource.MakeRaster(_tileId, null);
-			Assert.AreEqual(_api + "styles/v1/mapbox/satellite-v9/tiles/0/0/0", res1.GetUrl());
+			Assert.AreEqual(_api + "styles/v1/mapbox/satellite-v9/tiles/0/0/0", res1.GetUrl().Split("?".ToCharArray())[0]);
 
 			var res2 = TileResource.MakeRaster(_tileId, "mapbox://styles/mapbox/basic-v9");
-			Assert.AreEqual(_api + "styles/v1/mapbox/basic-v9/tiles/0/0/0", res2.GetUrl());
+			Assert.AreEqual(_api + "styles/v1/mapbox/basic-v9/tiles/0/0/0", res2.GetUrl().Split("?".ToCharArray())[0]);
 
 			var res3 = TileResource.MakeRaster(_tileId, "https://api.mapbox.com/styles/v1/penny/penny-map/tiles");
-			Assert.AreEqual(_api + "styles/v1/penny/penny-map/tiles/0/0/0", res3.GetUrl());
-			*/
+			Assert.AreEqual(_api + "styles/v1/penny/penny-map/tiles/0/0/0", res3.GetUrl().Split("?".ToCharArray())[0]);
 		}
 
+
 		[Test]
-		[Ignore("not working in Unity as 'TileResource' is 'internal sealed' and mapbox-sdk-cs and tests end up in 2 different aseemblies")]
 		public void GetUrlClassicRaster()
 		{
-			// TODO
-			/*
 			var res1 = TileResource.MakeClassicRaster(_tileId, null);
-			Assert.AreEqual(_api + "v4/mapbox.satellite/0/0/0.png", res1.GetUrl());
+			Assert.AreEqual(_api + "v4/mapbox.satellite/0/0/0.png", res1.GetUrl().Split("?".ToCharArray())[0]);
 
 			var res2 = TileResource.MakeClassicRaster(_tileId, "foobar");
-			Assert.AreEqual(_api + "v4/foobar/0/0/0.png", res2.GetUrl());
+			Assert.AreEqual(_api + "v4/foobar/0/0/0.png", res2.GetUrl().Split("?".ToCharArray())[0]);
 
 			var res3 = TileResource.MakeClassicRaster(_tileId, "test");
-			Assert.AreEqual(_api + "v4/test/0/0/0.png", res3.GetUrl());
-			*/
+			Assert.AreEqual(_api + "v4/test/0/0/0.png", res3.GetUrl().Split("?".ToCharArray())[0]);
 		}
 
 		[Test]
-		[Ignore("not working in Unity as 'TileResource' is 'internal sealed' and mapbox-sdk-cs and tests end up in 2 different aseemblies")]
 		public void GetUrlVector()
 		{
-			// TODO
-			/*
-			var res1 = TileResource.MakeVector(id, null);
-			Assert.AreEqual(api + "v4/mapbox.mapbox-streets-v7/0/0/0.vector.pbf", res1.GetUrl());
+			var res1 = TileResource.MakeVector(_tileId, null);
+			Assert.AreEqual(_api + "v4/mapbox.mapbox-streets-v7/0/0/0.vector.pbf", res1.GetUrl().Split("?".ToCharArray())[0]);
 
-			var res2 = TileResource.MakeVector(id, "foobar");
-			Assert.AreEqual(api + "v4/foobar/0/0/0.vector.pbf", res2.GetUrl());
+			var res2 = TileResource.MakeVector(_tileId, "foobar");
+			Assert.AreEqual(_api + "v4/foobar/0/0/0.vector.pbf", res2.GetUrl().Split("?".ToCharArray())[0]);
 
-			var res3 = TileResource.MakeVector(id, "test");
-			Assert.AreEqual(api + "v4/test/0/0/0.vector.pbf", res3.GetUrl());
-			*/
+			var res3 = TileResource.MakeVector(_tileId, "test");
+			Assert.AreEqual(_api + "v4/test/0/0/0.vector.pbf", res3.GetUrl().Split("?".ToCharArray())[0]);
 		}
+
+
+
 	}
 }
 
-#endif
 #endif
